a2e8155ffb6defcf451c92a5df9d067d16d9bec5
RuiwenX <rywn.xiong@gmail.com>
2024-03-20 08:50:27 -0400

    encoder main

---
+++ main.py
@@ -0,0 +1,39 @@
+def main():
+    password = None
+
+    while True:
+        print("Menu")
+        print("-------------")
+
+        # options
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        option = input("Please enter an option: ")
+
+        if option == "1":
+            password = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!")
+            password = encode(password)
+            print()
+
+        elif option == "2" and password is not None:
+            print(f'The encoded password is {password}, and the original password is {decode(password)}.')
+            print()
+
+        elif option == '3':
+            break
+
+
+def encode(password): # encoder: Ruiwen Xiong
+    num_list = []
+    num_list[:] = password
+    for i in range(len(password)):
+        num_list[i] = (int(num_list[i]) + 3) % 10
+    new_password = ""
+    for letter in num_list:
+        new_password += str(letter)
+
+    return new_password
